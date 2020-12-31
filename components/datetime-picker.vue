a72345352a2ec1cb533d08f8d1ec31c3989ed365
hsimao <e087754958@gmail.com>
2020-12-31 21:02:04 +0800

    add: custom datepicker

---
+++ components/datetime-picker.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+  name: 'DatetimePicker'
+}
+</script>
+
+<template>
+  <vue-date-picker class="datatime-picker" v-bind="$attrs" v-on="$listeners" />
+</template>
+
+<style lang="scss">
+$primaryColor: #005caf;
+@mixin flexCenter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.mx-time {
+  &-header {
+    height: 40px;
+    line-height: 40px;
+
+    &-title {
+      font-size: 16px;
+    }
+  }
+
+  &-content {
+    height: 250px;
+  }
+}
+
+.mx-calendar {
+  width: 290px;
+
+  &-content {
+    height: 244px;
+  }
+
+  &-header {
+    &-label .mx-btn {
+      font-size: 16px;
+    }
+
+    .mx-btn-icon-left,
+    .mx-btn-icon-right {
+      @include flexCenter;
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+    }
+
+    .mx-btn-icon-double-left,
+    .mx-btn-icon-double-right {
+      display: none;
+    }
+  }
+}
+
+.mx-table {
+  @include flexCenter;
+  flex-flow: column nowrap;
+
+  tbody {
+    margin-bottom: auto;
+  }
+
+  tr {
+    display: flex;
+    margin: 1px 0;
+  }
+
+  th {
+    @include flexCenter;
+    width: 32px;
+    margin: 0 2px;
+  }
+}
+
+.mx-date-row .cell {
+  @include flexCenter;
+  border-radius: 50%;
+  width: 32px;
+  margin: 0 2px;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,18 +10,14 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: ['~/assets/datepicker.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: ['~/plugins/vue2-datepicker'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -30,17 +26,17 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    '@nuxtjs/moment'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -56,6 +52,5 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {}
 }
